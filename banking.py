02d3f71703bf4270846198bdc440b17f1a2f49d8
shahad <shahad003.003@gmail.com>
2025-03-10 12:30:10 +0300

    project

--- banking.py
+++ banking.py
@@ -28,17 +28,15 @@ class Data:
             print(e)
 
 # 4.0 If Exists - ReadFile / Rows:
- try: 
-    with open("bank.csv", "r") as file:
-        contents = csv.DictReader(file)
-        for row in contents:
-            print(row) #will print: {'Name': 'The First Doctor', 'Actor': 'William Hartnell', 'Number of Episodes': '134'}
-            for prop in fieldnames:
-                print(row[prop]) # will print the value of each individual property
- except csv.Error as e:
-    print(e)
-
-
+#  try: 
+#     with open("bank.csv", "r") as file:
+#         contents = csv.DictReader(file)
+#         for row in contents:
+#             print(row) #will print: {'Name': 'The First Doctor', 'Actor': 'William Hartnell', 'Number of Episodes': '134'}
+#             for prop in fieldnames:
+#                 print(row[prop]) # will print the value of each individual property
+#  except csv.Error as e:
+#     print(e)
 
 class Bank:
     def __init__(self, name):
@@ -62,52 +60,100 @@ class Bank:
                print(e)
 
     def create_account(self):
-      first_name = input("Enter first name: ")
-      last_name = input("Enter last name: ")
-      password = input("Enter password: ")
-
-      print("Select the type of account(s):")
-      print("1. Checking account")
-      print("2. Savings account")
-      print("3. Both checking and savings accounts")
-
-      account_type = int(input("Enter the option number: "))
-      balance_checking = 0
-      balance_savings = 0
-      balance= True
-
-      while balance :
-        if account_type == 1 :
+      create_account = input(" Do you want create an account? y for yes n for no\n").lower()
+      if create_account == "y" :
+         first_name = input("Enter first name: ")
+         last_name = input("Enter last name: ")
+         password = input("Enter password: ")
+         print("Select the type of account(s):")
+         print("1. Checking account")
+         print("2. Savings account")
+         print("3. Both checking and savings accounts")
+         balance_checking = 0
+         balance_savings = 0
+         balance= True
+         while balance :
+          account_type = int(input("Enter the option number: "))
+          if account_type == 1 :
             balance_checking = float(input("Enter checking balance: "))
             balance=False
-        elif account_type == 2 :
+          elif account_type == 2 :
             balance_savings = float(input("Enter savings balance: "))
             balance=False
-        elif account_type == 3 :
+          elif account_type == 3 :
             balance_checking = float(input("Enter checking balance: "))
             balance_savings = float(input("Enter savings balance: "))
             balance=False
-        else :
+          else :
             print("Invalid option. No account created.")
-            account_type = float(input("Enter the option number: "))
             balance=True
             
-      new_customer = {
+         new_customer = {
         'account_id': self.account_id,
         'first_name': first_name,
         'last_name': last_name,
         'password': password,
         'balance_checking': balance_checking,
         'balance_savings': balance_savings
-        }
-      self.add_customer(customer=new_customer)
-      print(f"Account created successfully! Account ID: {self.account_id}")
+         }
+         self.add_customer(new_customer)
+         print(f"Account created successfully! Account ID: {self.account_id}")
+      else:
+         print("have nice day")
 
-
+class Customer_Login_Logout :
  
-bank = Bank("Golden Dune Bank")
+    def login(self):
+        idNumber = input ("Enter your Account ID: ")
+        username = input("Enter your first name: ")
+        password = input("Enter your password: ")
+        with open("bank.csv", "r") as bank_csv:
+            reader = csv.DictReader(bank_csv)
+            for customer in reader:
+                if customer["account_id"] == idNumber and customer["first_name"] == username and customer["password"] == password:
+                        print(f"Welcome, {customer['first_name']}! You are now logged in.\n")
+                        print("Account details:\n")
+                        print(f"{customer['account_id']}, {customer['first_name']}, {customer['last_name']}, {customer['balance_checking']}, {customer['balance_savings']}")
+                        return customer
+            else:
+             print("Invalid credentials. Please try again.")
 
+    def logout(self):
+       logout_account = input(" Do you want logout ? y for yes n for no\n").lower()
+       if logout_account =="y":
+          print("have nice day")
+#        else:
+#           print("No user logged in.")
+          
+# class Withdraw :
+#    def __init__(self, customer):
+#       self.customer=customer
+   
+#    def withdraw_money (self) :
+#       print(self.customer)
+
+    
+class Deposit :
+   pass
+ 	
+class Transfer : 
+   pass
+
+
+
+
+
+
+bank = Bank("Golden Dune Bank")
 bank.create_account()
+log=Customer_Login_Logout()
+log.login()
+log.logout()
+# withdraw = Withdraw(customer)
+# withdraw.withdraw_money()
+
+
+
 
 
 
